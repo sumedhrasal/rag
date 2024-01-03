e52444a18d1e6f67c066eb0fd5873db53172abc6
Sumedh Rasal <sumedh.rasal@here.com>
2024-01-02 23:12:42 -0600

    structured and unstructured methods created

--- rag/data.py
+++ rag/data.py
@@ -12,3 +12,23 @@ def load_documents_by_name(name):
         all_docs.extend(docs)
     return all_docs
 
+
+def get_structured_data_by_company(company_name):
+    # data to be filled using CrunchBase
+    response = {"Company": company_name, 
+                "Industry": "",
+                "Leadership": {
+                    "CEO": "",
+                    "CTO": "",
+                    "CFO": "",
+                    "C0O": ""
+                },
+                "Competitors": []}
+    return response
+
+
+def get_unstructured_data_by_company(company_name):
+    # data to be filled using crawlers
+    responses = []
+    return responses
+
